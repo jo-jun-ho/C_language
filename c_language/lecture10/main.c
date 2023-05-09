60da4a258f6e84788d36303a8f60f91e67762f3f
jo-jun-ho <jjh249275@gmail.com>
2023-05-09 15:41:53 +0900

    *파일 바꿈

---
+++ c_language/lecture10/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+	char hero[20]="김말이"; 
+	char heroin[20]="떡볶이"; 
+	char boss[20]="가래떡";
+
+	printf("\n\n\t***  난 네가 싫어 ***\n\n\n");
+	printf("%s는 %s에게 고백을 했다..",hero,heroin);
+	printf("%s(이)가 %s의 뺨을 세게 때렸다...",boss,hero);
+	printf("시작은 했는데 수습이 안된다...");
+	return 0;
+}
